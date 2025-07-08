2b6e21e7b4688701fa2a11758437f8a616227330
Ruslana Yakymiv <ruslanayakymiv@gmail.com>
2025-07-08 22:29:38 +0300

    Додано варіанти і оновлено код homework3.js

--- homework3.js
+++ homework3.js
@@ -1,17 +1,60 @@
-export const pow = (x, y) => {
-  if (typeof x !== 'number' || typeof y !== 'number' || y < 0 || !Number.isInteger(y)) {
-    return NaN;
-  }
+// Варіант 1
 
+function pow(x, y) {
   let result = 1;
 
   for (let i = 0; i < y; i++) {
-    result *= x;
+    result = result * x;
   }
 
   return result;
-};
+}
 
-console.log(pow(2, 3));  // 8
-console.log(pow(5, 0));  // 1
-console.log(pow(3, 2));  // 9
+// Перевірка
+console.log(pow(2, 3));
+console.log(pow(5, 2));
+console.log(pow(7, 0));
+
+// Варіант 2
+
+
+// function pow(x, y) {
+//   if (typeof x !== 'number' || typeof y !== 'number' || y < 0 || !Number.isInteger(y)) {
+//     return NaN;
+//   }
+
+//   let result = 1;
+//   for (let i = 0; i < y; i++) {
+//     result *= x;
+//   }
+//   return result;
+// }
+// // Перевірка
+// console.log(pow(2, 3));
+// console.log(pow(5, 0));
+// console.log(pow(3, 1));
+// console.log(pow(2, -1));
+// console.log(pow('2', 3));
+// console.log(pow(2, 3.5));
+
+
+
+// Варіант 3
+
+// export const pow = (x, y) => {
+//   if (typeof x !== 'number' || typeof y !== 'number' || y < 0 || !Number.isInteger(y)) {
+//     return NaN;
+//   }
+
+//   let result = 1;
+
+//   for (let i = 0; i < y; i++) {
+//     result *= x;
+//   }
+
+//   return result;
+// };
+
+// console.log(pow(2, 3));  // 8
+// console.log(pow(5, 0));  // 1
+// console.log(pow(3, 2));  // 9
